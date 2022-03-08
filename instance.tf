e3e4bb54a00423af74a63c53f8c57695dfb5fed0
smit-darji <smit.softvan@gmail.com>
2022-03-08 14:27:51 +0530

    fddf

--- instance.tf
+++ instance.tf
@@ -2,19 +2,19 @@
 resource "aws_instance" "app_server" {
   ami = var.image_id
   //ami                    = data.aws_ami.ubuntu.id#copy from aws server name
-  instance_type          = var.instance_type #instance type
- // key_name               = aws_key_pair.key-tf.key_name
+  instance_type = var.instance_type #instance type
+  // key_name               = aws_key_pair.key-tf.key_name
   vpc_security_group_ids = ["${aws_security_group.terraform-sg-smit.id}"]
   tags = {
     Name = "Smit-First-Terraform" #name of insatnce
   }
   user_data = file("${path.module}/script.sh")
 
-  connection {                                  //provisioner ne conncetion joie to globaly lai lese
-    type        = "ssh"                         //type
-    user        = "ubuntu"                      // server
-   // private_key = file("${path.module}/id_rsa") //get key
-    host        = self.public_ip                //for get ip address public
+  connection {      //provisioner ne conncetion joie to globaly lai lese
+    type = "ssh"    //type
+    user = "ubuntu" // server
+    // private_key = file("${path.module}/id_rsa") //get key
+    host = self.public_ip //for get ip address public
   }
   provisioner "file" {
     source = "readme.md" //teraaform muchine 
